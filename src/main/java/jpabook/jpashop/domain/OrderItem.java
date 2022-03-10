3ec34652de42b0e8e0d4fbfcd7894af86d7ba295
jaeho-jang <jaehojang@nate.com>
2022-03-10 13:23:06 +0900

    Add methods to OrderItem

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -21,14 +22,36 @@ public class OrderItem {
 
     @ManyToOne
     @JoinColumn(name = "item_id")
+    @Setter
     private Item item;
 
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
+    @Setter
     private int orderPrice;
 
+    @Setter
     private int count;
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    public void cancel() {
+        item.addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 
 }

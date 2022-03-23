c97f195971430c5c9065f48e19be3515944e3953
jaeho-jang <jaehojang@nate.com>
2022-03-23 23:57:13 +0900

    Add order test

---
+++ src/test/java/jpabook/jpashop/Service/OrderServiceTest.java
@@ -0,0 +1,69 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+
+    @Test
+    public void 상품주문() throws Exception {
+        Member member = Member.builder()
+                .name("회원1")
+                .address(new Address("서울", "영등포", "123-123"))
+                .build();
+
+        em.persist(member);
+
+        Book book = Book.builder()
+                .name("JPA")
+                .price(10000)
+                .stockQuantity(10)
+                .build();
+
+        em.persist(book);
+
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        Order order = orderRepository.findOne(orderId);
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, order.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다", 1, order.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다", 10000 * orderCount, order.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다", 8, book.getStockQuantity());
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+
+    }
+
+    @Test
+    public void 재고수량초과() throws Exception {
+
+    }
+
+}
